aa539cd38b11ff04f33fe94fc29d48dc3c6f4cad
Ayham <ayhammhedat5@gmail.com>
2026-05-25 03:55:57 +0300

    Add authentication backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
+const authRoutes = require("./routes/authRoutes")
 
 dotenv.config()
 
@@ -15,6 +16,8 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("StudyHub API is running")
 })
+
+app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT || 5000
 
