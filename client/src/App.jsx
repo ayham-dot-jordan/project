2aae6723a4c3bfa6c3cf95abf1bfa98f190d1c37
Ayham <ayhammhedat5@gmail.com>
2026-05-26 20:27:32 +0300

    Add protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Courses from "./pages/Courses";
-import Tasks from "./pages/Tasks";
-import Notes from "./pages/Notes";
-import Profile from "./pages/Profile";
+import { Navigate, Routes, Route } from "react-router-dom"
+import Landing from "./pages/Landing"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import Courses from "./pages/Courses"
+import Tasks from "./pages/Tasks"
+import Notes from "./pages/Notes"
+import Profile from "./pages/Profile"
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token")
+
+  if (!token) {
+    return <Navigate to="/login" />
+  }
+
+  return children
+}
 
 function App() {
   return (
@@ -14,13 +24,53 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/courses" element={<Courses />} />
-      <Route path="/tasks" element={<Tasks />} />
-      <Route path="/notes" element={<Notes />} />
-      <Route path="/profile" element={<Profile />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/courses"
+        element={
+          <ProtectedRoute>
+            <Courses />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/tasks"
+        element={
+          <ProtectedRoute>
+            <Tasks />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/notes"
+        element={
+          <ProtectedRoute>
+            <Notes />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
